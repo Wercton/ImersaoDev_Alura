d8d9d5e807d3410bff7bd9aff73c5752bf7ccb93
Wercton <werctonmatheus@gmail.com>
2021-04-02 15:37:06 -0300

    Terceiro projeto da imersão Alura

---
+++ Projeto 3: Mentalista/script.js
@@ -0,0 +1,21 @@
+/*var numeroSecreto = parseInt(Math.random() * 10)
+var tentativas = 3
+var resposta = window.document.getElementById("reposta")
+
+while (tentativas > 0) {
+    var chute = parseInt(document.getElementById("chute"))
+
+    if (numeroSecreto == chute) {
+        resposta.innerHTML = "Parabéns, você acertou!"
+        break
+    } else if (tentativas == 1) {
+        resposta.innerHTML = "Você perdeu todas as chances! O número secreto era " + numeroSecreto + "."
+        break
+    } else if (chute > numeroSecreto) {
+        resposta.innerHTML = "O número secreto é menor que " + chute
+        tentativas = tentativas - 1
+    } else if (chute < numeroSecreto) {
+        resposta.innerHTML = "O número secreto é maior que " + chute
+        tentativas = tentativas - 1
+    }
+}*/
